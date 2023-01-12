94c932faf06f1dbc5f63494f66c585891d32e80a
Vincent Lepelley <vincent@lepelley.fr>
2023-01-12 02:56:49 +0100

    fix home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\UserPokedexRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/', name: 'app_home')]
 class HomeController extends AbstractController
 {
-    public function __invoke(): Response
+    public function __construct(private readonly UserPokedexRepository $repository) {}
+
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('home.html.twig', [
+            'user_pokedex' => $this->repository->findAll(),
         ]);
     }
 }
