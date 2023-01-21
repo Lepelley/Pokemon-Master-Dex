7c456ad0296d34958885a373ff714464860a6e9d
Vincent Lepelley <vincent@lepelley.fr>
2023-01-21 19:00:27 +0100

    fix : typo

--- src/Controller/Admin/Ball/ListController.php
+++ src/Controller/Admin/Ball/ListController.php
@@ -7,10 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin/ball/', name: 'app_admin_ball_list')]
 class ListController extends AbstractController
 {
-    #[Route('/admin/ball/', name: 'app_admin_ball_list')]
-    public function index(BallRepository $repository): Response
+    public function __invoke(BallRepository $repository): Response
     {
         return $this->render('admin/ball/list.html.twig', [
             'balls' => $repository->findAll(),
